3666fbd10a96b7eea088ccb77c9bfab408ef3938
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-02-23 09:44:37 +0100

    Face steering reimplemented with AIController
    Signed-off-by: Khine LIN KYAW <khine.lin.kyaw@student.howest.be>

--- Source/GameAIProg/Movement/SteeringBehaviors/SteeringAgent.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/SteeringAgent.cpp
@@ -2,49 +2,57 @@
 
 #include "SteeringAgent.h"
 
+#include "AIController.h"
+
 
 // Sets default values
 ASteeringAgent::ASteeringAgent()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
 void ASteeringAgent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 void ASteeringAgent::BeginDestroy()
 {
-	Super::BeginDestroy();
+    Super::BeginDestroy();
 }
 
 // Called every frame
 void ASteeringAgent::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
-	if (SteeringBehavior)
-	{
-		SteeringOutput const Output = SteeringBehavior->CalculateSteering(DeltaTime, *this);
-		
-		AddMovementInput(FVector{Output.LinearVelocity, 0.f});
-		
-		FRotator NewRotation = FRotator{0.f, 5.f, 0.f} * Output.AngularVelocity;
-		AddActorLocalRotation(NewRotation);
-	}
+    if (SteeringBehavior)
+    {
+        if (SteeringOutput const Output = SteeringBehavior->CalculateSteering(DeltaTime, *this); Output.IsValid) 
+        {
+            if (AAIController* AaiController { Cast<AAIController>(GetController())})
+            {
+                AddMovementInput(FVector{Output.LinearVelocity, 0.f});
+
+                FRotator NewRotation { GetActorRotation() };
+                NewRotation.Yaw += Output.AngularVelocity * GetMaxAngularSpeed() * DeltaTime;
+
+                AaiController->SetControlRotation(NewRotation);
+                FaceRotation(NewRotation);
+            }
+        }
+    }
 }
 
 // Called to bind functionality to input
 void ASteeringAgent::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
 void ASteeringAgent::SetSteeringBehavior(ISteeringBehavior* NewSteeringBehavior)
 {
-	SteeringBehavior = NewSteeringBehavior;
+    SteeringBehavior = NewSteeringBehavior;
 }
-
